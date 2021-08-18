8fbc62c0d33b4f1a0fe4f93f07af099dc1089ab6
Dmitry Zakharov <zakharov.dy@mail.ru>
2021-08-18 14:25:02 +0500

    upd expo to 0.42

--- App.tsx
+++ App.tsx
@@ -1,67 +1,24 @@
-import {AppLoading, Asset, Font, Icon} from 'expo';
-import {Provider} from 'mobx-react';
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Platform, StatusBar, StyleSheet, View} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import stores from './mobx';
-import AppNavigator from './navigation/AppNavigator';
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
-export default class App extends React.Component {
-  public state = {
-    isLoadingComplete: false
-  };
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-  public render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <Provider {...stores}>
-            <AppNavigator />
-          </Provider>
-        </View>
-      );
-    }
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
   }
-
-  public _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png')
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
-      })
-    ]);
-  };
-
-  public _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  public _handleFinishLoading = () => {
-    this.setState({isLoadingComplete: true});
-  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  }
-});
--- hoc/bindStore.tsx
+++ hoc/bindStore.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import React, { useEffect } from 'react';
 import {StyleProp, View, ViewStyle} from 'react-native';
-import {NavigationEvents} from 'react-navigation';
 
 import {BoundStore} from '../types/core/boundStore';
 
@@ -8,28 +8,32 @@ interface Options {
   containerStyle?: StyleProp<ViewStyle>;
 }
 
-function bindStore<P, T extends BoundStore>(boundStore: T, options: Options) {
-  return function (Component: React.ComponentType<P & { boundStore: T }>) {
-    class BindStore extends React.Component<P> {
-      private onWillBlur = () => boundStore.toFinish();
-      private onWillFocus = () => boundStore.startUp();
-
-      public render() {
-        const {containerStyle} = options;
-        return (
-          <View style={containerStyle}>
-            <NavigationEvents
-              onWillBlur={this.onWillBlur}
-              onWillFocus={this.onWillFocus}
-            />
-            <Component {...this.props} boundStore={boundStore} />
-          </View>
-        );
-      }
-    }
-
-    return BindStore;
-  };
+interface Options {
+  containerStyle?: StyleProp<ViewStyle>;
 }
 
-export default bindStore;
+export default function bindStore<P, T extends BoundStore>(boundStore: T, options: Options) {
+  return function (Component: React.ComponentType<P & { boundStore: T }>) {
+    return function BindStore(props: P) {
+      const isFocused = useIsFocused();
+      useEffect(() => {
+        if(!isFocused && boundStore.toFinish){
+          boundStore.toFinish();
+          return;
+        }
+
+        if(isFocused && boundStore.startUp){
+          boundStore.startUp();
+          return;
+        }
+      }, [isFocused]);
+
+      const containerStyle = options && options.containerStyle;
+      return (
+        <View style={containerStyle}>
+          <Component {...props} boundStore={boundStore} />
+        </View>
+      );
+    };
+  };
+}
